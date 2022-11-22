14193ab7759283fb018eea57879bc39395d45218
Danilo Costa <117859128+danilofcosta@users.noreply.github.com>
2022-11-21 21:00:08 -0300

    Update ffmpeg.py
    Add  TRATAMENTO DE POSSIVEIS ERROS caso o Scrip não funcione

--- ffmpeg.py
+++ ffmpeg.py
@@ -7,8 +7,18 @@ import os
 from pyunpack import Archive
 
 os.mkdir('Descompatado')#criar uma pasta
-#local do arquivo com  extenção 7z
+#Lcal do arquivo com  extenção .z
 aquivocomcpato=q.7z
 Archive (aquivocomcpato).extractall('Descompatado') #extrai aquivo para a pasta ex
 os.rename("ex\\ffmpeg-2022-10-27-git-e0b03331ae-full_build", "ex\\ffmpeg")#renomea pasta
 os.system("setx path systemdrive\\ffmpeg\\binx") #coloca o ffmpeg como variavel de ambiente do compudor
+
+
+'''
+TRATAMENTO DE POSSIVEIS ERROS
+- Extrair aquivo
+- Dentro do aquivo a pasta 'bin' contem o aquivos nessesario para conpara o funcionamento do pydub
+- Copie o caminho do arquivo
+- Vai em painel de controle >Pesquise criar ponto de restauração e clique >Menu superior > Proteção do sistema> abaixo'Variáveis de Ambiente'> path adicione o diretorio na lista de variaveis 
+
+'''
